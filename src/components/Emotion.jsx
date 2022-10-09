1bc386e4234e5cf8bad3f3f6ba97265ce961406e
N2-Met <yuyushiki11@gmail.com>
2022-10-09 22:53:09 +0900

    Sunday

--- src/components/Emotion.jsx
+++ src/components/Emotion.jsx
@@ -1,6 +1,19 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from "@emotion/react";
+
 export const Emotion = () => {
+    //scssの書き方がそのまま可能な書き方
+    const containerStyle = css`
+    border: solid 1px #aaa;
+    border-radius: 20px;
+    padding: 8px;
+    margin: 8px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`
     return (
-        <div>
+        <div css={containerStyle}>
             <p>これはEmotionの場合。</p>
             <button>ボタン</button>
         </div>
